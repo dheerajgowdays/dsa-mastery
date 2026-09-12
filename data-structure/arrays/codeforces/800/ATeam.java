9d725a20b3d0892126108189e98018d24b8ad285
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-09-12 23:43:38 +0530

    feat:Sloved Team problem

---
+++ data-structure/arrays/codeforces/800/ATeam.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ATeam{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) return;
+
+        int n = sc.nextInt();
+        int solvedCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if (a + b + c >= 2) {
+                solvedCount++;
+            }
+        }
+
+        System.out.println(solvedCount);
+        sc.close();
+    }
+}
